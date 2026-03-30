b3a21f5be89d77362e21dd7d857735c3c4e5ca1d
nitonise <nisenito@yahoo.com>
2026-03-30 07:49:05 +0200

    Add spinner

--- src/app/components/todo-board/todo-board.ts
+++ src/app/components/todo-board/todo-board.ts
@@ -1,14 +1,18 @@
-import { ChangeDetectionStrategy, Component, computed, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from '@angular/core';
 import { TodoService } from 'src/app/services/todo-service';
 import { Todo } from 'src/app/models/Todo';
+import { AppSpinner } from 'src/app/components/app-spinner/app-spinner';
 
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'app-todo-board',
     templateUrl: './todo-board.html',
+    imports: [
+        AppSpinner
+    ]
 })
-export class TodoBoard {
+export class TodoBoard implements OnInit {
 
     private readonly todoService = inject(TodoService);
 
@@ -21,4 +25,10 @@ export class TodoBoard {
         const todoList = this.todoService.todoList();
         return todoList.filter(t => t.status === 'COMPLETED');
     });
+
+    protected readonly isLoading = signal<boolean>(true);
+
+    ngOnInit() {
+        setTimeout(() => this.isLoading.set(false), 1000);
+    }
 }
