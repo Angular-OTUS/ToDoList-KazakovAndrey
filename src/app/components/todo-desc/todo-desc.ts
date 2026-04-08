064bd0deb506f0c314912fca2dde1eaf0b2ddf09
nitonise <nisenito@yahoo.com>
2026-04-08 20:50:40 +0200

    Update status

--- src/app/components/todo-desc/todo-desc.ts
+++ src/app/components/todo-desc/todo-desc.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
 import { MatFormField, MatInput } from '@angular/material/input';
 import { TodoService } from 'src/app/services/todo-service';
+import { AppHint } from 'src/app/directives/app-hint';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { TodoStatus } from 'src/app/models/TodoStatus';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -8,19 +11,32 @@ import { TodoService } from 'src/app/services/todo-service';
     imports: [
         MatFormField,
         MatInput,
+        AppHint,
+        MatCheckbox,
     ],
     templateUrl: './todo-desc.html',
 })
 export class TodoDesc {
 
+    private readonly todoService = inject(TodoService);
+
     protected readonly todoId = input<string>();
 
-    protected readonly todoDesc = computed(() => {
+    protected readonly todo = computed(() => {
         const id = this.todoId();
         const list = this.todoService.todoList();
 
-        return list.find(t => t.id === id)?.description ?? '';
-    });
+        return list.find(t => t.id === id);
+    })
 
-    private readonly todoService = inject(TodoService);
+    protected onTodoChecked(checked: boolean) {
+        const todo = this.todo();
+        if (!todo) {
+            return;
+        }
+
+        const status: TodoStatus = checked ? 'COMPLETED' : 'IN_PROGRESS'
+
+        this.todoService.updateTodoStatus(todo.id, { status })
+    }
 }
